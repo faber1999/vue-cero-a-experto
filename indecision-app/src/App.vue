def4a24f247e51d6f1db747710a1a1967d9b7a6b
Faber Grajales <fabergrajales1999@gmail.com>
2025-02-20 08:49:48 -0500

    feat: Initial commit indesicion app porject

---
+++ indecision-app/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import MyCounter from './components/MyCounter.vue';
+
+</script>
+
+<template>
+  <h1>Mi primera app</h1>
+  <hr>
+  <MyCounter />
+</template>
---
+++ indecision-app/src/components/MyCounter.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+const counter = ref(0)
+const squareCounter = computed(() => counter.value ** 2)
+</script>
+
+<template>
+  <section>
+    <h3>Counter: {{ counter }}</h3>
+    <h3>Square: {{ squareCounter }}</h3>
+
+    <div>
+      <button @click="counter--">-1</button>
+      <button @click="counter++">+1</button>
+    </div>
+  </section>
+</template>
